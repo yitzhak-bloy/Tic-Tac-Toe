d3c8c95642959b0dac2526893cb49773e4d8cd65
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2019-02-20 21:47:22 +0200

    add

--- src/Game.js
+++ src/Game.js
@@ -57,7 +57,7 @@ class Game extends React.Component {
 
     let status;
     if (winner) {
-        status = 'Winner: ' + winner;
+        status = 'Winner: ' + winner + '. Congratulations!';
     } else {
         status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
