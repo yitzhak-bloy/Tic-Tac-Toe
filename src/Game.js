f55867ad60c0075eeff5e828199455a95b308890
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2019-06-06 09:58:54 +0300

    A tiny change

--- src/Game.js
+++ src/Game.js
@@ -57,7 +57,7 @@ class Game extends React.Component {
 
     let status;
     if (winner) {
-        status = 'the great winner is: ' + winner + ', Congratulations!';
+        status = 'the winner is: ' + winner + ', Congratulations!';
     } else {
         status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
