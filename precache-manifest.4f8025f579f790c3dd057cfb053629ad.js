8a95608251c5ade5469263528f4014adfc589767
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2019-10-05 20:58:36 +0300

    Updates

---
+++ precache-manifest.4f8025f579f790c3dd057cfb053629ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3860bc0ca4d46efb440f",
+    "url": "/static/js/main.3860bc0c.chunk.js"
+  },
+  {
+    "revision": "abd9e857442f04b96f9b",
+    "url": "/static/js/1.abd9e857.chunk.js"
+  },
+  {
+    "revision": "3860bc0ca4d46efb440f",
+    "url": "/static/css/main.9811ddba.chunk.css"
+  },
+  {
+    "revision": "1389ca49840180a37af120c81c91bc61",
+    "url": "/index.html"
+  }
+];
